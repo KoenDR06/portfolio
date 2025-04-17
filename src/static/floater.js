918b0d49e88e1e24c4d6eee5a4eda61d0f417543
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-04-17 22:36:58 +0200

    added beam to fixture

--- src/static/floater.js
+++ src/static/floater.js
@@ -1,7 +1,16 @@
+function clamp(value, min, max) { return Math.max( min, Math.min(value, max) ); }
+
+const body = document.querySelector('body');
+const beams = document.querySelector(".beams");
+
 window.addEventListener("scroll", function(){
     const floaters = document.querySelectorAll(".floater");
-    const yPos = 0 - window.scrollY;
+
+    const yPos = window.scrollY / window.innerHeight;
     for (const floater of floaters) {
-        floater.style.top = -yPos/20 + "%";
+        floater.style.top = yPos*50 + "%";
     }
+
+    beams.style.opacity = 1-2*yPos;
+    beams.style.display = 1-2*yPos < 0 ? 'none' : 'block'
 });
